25b67997d9675ee96727950f73d65e5ed694f181
Declin-Anderson <Declin-Anderson@users.noreply.github.com>
2023-03-03 02:49:53 -0600

    Unit movement and Selection Done
    You can select units now and it hightlights them, as well as you can move them independently, the next step is making a grid system and updating the camera.

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+
+    public static UnitActionSystem Instance {get; private set;}
+
+    public event EventHandler OnSelectedUnitChanged;
     // Current unit selected by the player
     [SerializeField] private Unit selectedUnit;
     // The layer that the units are on
     [SerializeField] private LayerMask unitLayerMask;
+
+    private void Awake() 
+    {
+        if(Instance != null)
+        {
+            Debug.LogError("There's more than one UnitActionSystem!" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
 
     private void Update() 
     {
@@ -28,10 +44,23 @@ public class UnitActionSystem : MonoBehaviour
         {
             if(raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
         return false;
+    }
+
+    // Sets the unit that is clicked on as the selected unit and invokes the event
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
